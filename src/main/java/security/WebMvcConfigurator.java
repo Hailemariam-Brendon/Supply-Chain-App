3d89aac0a5c76cb20f75b542bfb1001b6bb622e9
Hailemariam Brendon <hailemariam.brendon@gmail.com>
2026-06-26 09:54:55 -0500

    I added a view controller registery to the WebMvcConfigurator file and created a home page.

--- src/main/java/security/WebMvcConfigurator.java
+++ src/main/java/security/WebMvcConfigurator.java
@@ -1,6 +1,11 @@
 package security;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebMvcConfigurator implements WebMvcConfigurer {
+    public  void addViewControllers(ViewControllerRegistry registery){
+        registery.addViewController("/home").setViewName("home");
+
+    }
 }
